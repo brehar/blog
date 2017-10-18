ac69e5abab59c979976c7a3fdf4648f694107c9d
Brett Hartman <brett.hartman@mac.com>
2017-10-18 12:30:14 -0700

    Incorporate delete action

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import { FETCH_POSTS } from '../actions/index';
 import { FETCH_POST } from '../actions/index';
+import { DELETE_POST } from '../actions/index';
 
 export default function(state = {}, action) {
 	switch (action.type) {
@@ -9,6 +10,8 @@ export default function(state = {}, action) {
 			return _.mapKeys(action.payload.data, 'id');
 		case FETCH_POST:
 			return { ...state, [action.payload.data.id]: action.payload.data };
+		case DELETE_POST:
+			return _.omit(state, action.payload);
 		default:
 			return state;
 	}
